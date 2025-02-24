3bd46d7b3ba7506963aa3f3c5d3964c8552915a8
Jake Dennison <jake.dennison@hotmail.co.uk>
2025-02-24 22:57:08 +0000

    updated readme and added feature components

--- components/FeatureListner.tsx
+++ components/FeatureListner.tsx
@@ -1,16 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLDClient } from 'launchdarkly-react-client-sdk';
 
 export default function FeatureListener() {
+  const [flagValue, setFlagValue] = useState(false);
   const ldClient = useLDClient();
 
   useEffect(() => {
     if (!ldClient) return;
 
-    ldClient.on('change:newFeature', (flagValue) => {
-      console.log(`Feature Flag Updated: ${flagValue}`);
-    });
+    // Set initial flag value
+    const initialFlagValue = ldClient.variation('newFeature', false); // Set default as false
+    setFlagValue(initialFlagValue);
+
+    // Listen for changes to the feature flag
+    const flagChangeListener = (newFlagValue: boolean) => {
+      console.log(`Feature Flag Updated: ${newFlagValue}`);
+      setFlagValue(newFlagValue); // Update UI state when the flag changes
+    };
+
+    ldClient.on('change:newFeature', flagChangeListener);
+    return () => {
+    };
   }, [ldClient]);
 
-  return null;
+  return (
+    <div>
+      {flagValue ? (
+        <p className="text-green-500">🚀 Feature is active!</p>
+      ) : (
+        <p className="text-red-500">🔒 Feature is inactive.</p>
+      )}
+    </div>
+  );
 }
